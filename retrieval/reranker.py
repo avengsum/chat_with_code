89778fc72464d512cdeea108c31ad871bf4d5f19
avengsum <sy710481@gmail.com>
2026-01-02 00:08:23 +0530

    did some changes in rerank

--- retrieval/reranker.py
+++ retrieval/reranker.py
@@ -2,7 +2,7 @@ from langchain_classic.retrievers import ContextualCompressionRetriever
 from langchain_community.document_compressors.flashrank_rerank import FlashrankRerank
 from .retriever import hybrid_retriever
 
-compressor = FlashrankRerank()
+compressor = FlashrankRerank(top_n=5)
 
 advance_retriever = ContextualCompressionRetriever(
   base_compressor = compressor,
